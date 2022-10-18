ac759412344ddd429e86adf76a756dd27381d019
TinMon11 <tincho_mondino@hotmail.com>
2022-10-18 16:56:18 -0300

    Correciones en btn css. Metodo Try-Catch en js.

--- metamask.js
+++ metamask.js
@@ -10,16 +10,22 @@ async function accessToMetamask() {
 
     if (typeof window.ethereum.isMetaMask) {
 
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-        const account = accounts[0];
-        adress.innerHTML = `Login Adress ${account}`
-        navbarUser.innerText = `${account}`
-        loginButton.disabled=true;
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            const account = accounts[0];
+            adress.innerHTML = `Login Adress ${account}`
+            navbarUser.innerText = `${account}`
+            loginButton.disabled = true;
+
+        } catch (error) {
+            new swal("Error connecting to Metamask", `${error}`, "error");
+        }
 
     } else {
         new swal("MetaMask Not Installed", "Please install MetaMask extension for Chrome", "error");
     }
 }
+
 
 
 
