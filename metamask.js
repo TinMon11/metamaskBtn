95306c13fc4dabfa89c5f09a3e22e2038d2240fe
TinMon11 <tincho_mondino@hotmail.com>
2022-10-18 16:01:57 -0300

    Avances en HomePage

--- metamask.js
+++ metamask.js
@@ -1,7 +1,10 @@
 
-
 let adress = document.getElementById("accountArea")
+let loginButton = document.getElementById("loginButton")
+let navbarUser = document.getElementById("navbarLogin")
 
+// const express = require("express");
+// const path = require("path");
 
 async function accessToMetamask() {
 
@@ -9,8 +12,10 @@ async function accessToMetamask() {
 
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         const account = accounts[0];
-        adress.innerHTML = `Login Adress
-         ${account}`
+        adress.innerHTML = `Login Adress ${account}`
+        navbarUser.innerText = `${account}`
+        loginButton.disabled=true;
+
     } else {
         new swal("MetaMask Not Installed", "Please install MetaMask extension for Chrome", "error");
     }
